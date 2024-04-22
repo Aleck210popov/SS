3b9e4f7f9422bf0bd1a4a40b0123b2ce75961e12
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2024-04-22 16:11:16 +0300

    start GraphicalInterface2

---
+++ src/com/ss/main/TestTriangle.java
@@ -0,0 +1,45 @@
+package com.ss.main;
+
+import com.ss.one.Triangle;
+
+public class TestTriangle
+{
+  public static void main (String[]args)
+  {
+	Triangle t1 = null, t2 = null, t3 = null, t4 = null, t5 = null;
+	  try
+	{
+	  t1 = new Triangle ();
+	  t2 = new Triangle (1.5);
+	  t3 = new Triangle (2, 2);
+	  t4 = new Triangle (3, 4, 5);
+	  t5 = new Triangle (1, 2, 2);
+	}
+	catch (IllegalArgumentException e)
+	{
+	  System.err.printf ("Unable to create a triangle: " + e.getMessage ());
+	  System.exit (-1);
+	}
+	System.out.println ("t1 has sides: " + t1);
+	System.out.println ("t2 has sides: " + t2);
+	System.out.println ("t3 has sides: " + t3);
+	System.out.println ("t4 has sides: " + t4);
+	System.out.println ("t5 has sides: " + t5);
+	System.out.println ("t3 = t4 is " + t3.equals (t4));
+	System.out.println ("t3 = t5 is " + t3.equals (t5));
+	System.out.println ("t1's angles are: " + t1.angle1 () + ", " +
+						t1.angle2 () + ", " + t1.angle3 ());
+	System.out.println ("t4's angles are: " + t4.angle1 () + ", " +
+						t4.angle2 () + ", " + t4.angle3 ());
+	System.out.println ("t1's perimeter is: " + t1.perimeter ());
+	System.out.println ("t1's surface is: " + t1.surface ());
+	System.out.println ("t1's heights are: " + t1.height1 () + ", " +
+						t1.height2 () + ", " + t1.height3 ());
+	System.out.println ("t1's equilaterality is " + t1.isEquilateral ());
+	System.out.println ("t4's equilaterality is " + t4.isEquilateral ());
+	System.out.println ("t1's isoscelesness is " + t1.isIsosceles ());
+	System.out.println ("t4's isoscelesness is " + t4.isIsosceles ());
+	System.out.println ("t1's rectangularity is " + t1.isRectangular ());
+	System.out.println ("t4's rectangularity is " + t4.isRectangular ());
+  }
+}
---
+++ src/com/ss/one/Triangle.java
@@ -0,0 +1,146 @@
+package com.ss.one;
+
+public class Triangle
+{
+  private double side1;
+  private double side2;
+  private double side3;
+  private double temp;
+
+  public Triangle ()
+  {
+	this (1, 1, 1);
+  }
+  public Triangle (double s1)
+  {
+	this (s1, 1, 1);
+  }
+  public Triangle (double s1, double s2)
+  {
+	this (s1, s2, 1);
+  }
+
+  public Triangle (double s1, double s2, double s3)
+  {
+	side1 = Math.max (Math.max (s1, s2), s3);
+	side3 = Math.min (Math.min (s1, s2), s3);
+	side2 = s1 + s2 + s3 - side1 - side3;
+	if (side3 <= 0)
+	  {
+		throw new IllegalArgumentException ("Sides must be positive");
+	  }
+	if (side1 >= side2 + side3)
+	  {
+		throw new
+		  IllegalArgumentException
+		  ("Any side must be less than the sum of two remaining ones");
+	  }
+  }
+
+  public String toString ()
+  {
+	return side1 + ", " + side2 + ", " + side3;
+  }
+
+  public boolean equals (Triangle a)
+  {
+	if ((this.side1 == a.side1) && (this.side2 == a.side2)
+		&& (this.side3 == a.side3))
+	  {
+		return true;
+	  }
+	else
+	  {
+		return false;
+	  }
+  }
+
+  public double getSide1 ()
+  {
+	return side1;
+  }
+
+  public double getSide2 ()
+  {
+	return side2;
+  }
+
+  public double getSide3 ()
+  {
+	return side3;
+  }
+
+  public void setSide1 (double n)
+  {
+	side1 = n;
+  }
+
+  public void setSide2 (double n)
+  {
+	side2 = n;
+  }
+
+  public void setSide3 (double n)
+  {
+	side3 = n;
+  }
+
+  public double angle1 ()
+  {
+	return Math.acos ((side2 * side2 + side3 * side3 - side1 * side1) /
+					  (2 * side2 * side3)) * 180 / Math.PI;
+  }
+
+  public double angle2 ()
+  {
+	return Math.acos ((side1 * side1 + side3 * side3 - side2 * side2) /
+					  (2 * side1 * side3)) * 180 / Math.PI;
+  }
+
+  public double angle3 ()
+  {
+	return Math.acos ((side2 * side2 + side1 * side1 - side3 * side3) /
+					  (2 * side2 * side1)) * 180 / Math.PI;
+  }
+
+  public double perimeter ()
+  {
+	return side1 + side2 + side3;
+  }
+
+  public double surface ()
+  {
+	double p = perimeter () / 2;
+	return Math.sqrt (p * (p - side1) * (p - side2) * (p - side3));
+  }
+
+  public double height1 ()
+  {
+	return 2 * surface () / side1;
+  }
+
+  public double height2 ()
+  {
+	return 2 * surface () / side2;
+  }
+
+  public double height3 ()
+  {
+	return 2 * surface () / side3;
+  }
+
+  public boolean isEquilateral ()
+  {
+	return (side1 == side3);
+  }
+
+  public boolean isIsosceles ()
+  {
+	return (side1 == side2) || (side2 == side3);
+  }
+
+  public boolean isRectangular ()
+  {
+	return (Math.abs (angle1 () - 90) <= 0.000001);
+  }
+}
